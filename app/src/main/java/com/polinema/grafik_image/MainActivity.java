2aa5ad9a926a9b22cd3a7f04e2405f7a620811d2
amaliadama <amaliadamayanti6@gmail.com>
2019-11-01 15:14:39 +0700

    source code selesai

--- app/src/main/java/com/polinema/grafik_image/MainActivity.java
+++ app/src/main/java/com/polinema/grafik_image/MainActivity.java
@@ -3,9 +3,11 @@ package com.polinema.grafik_image;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.widget.Button;
+
 
 public class MainActivity extends Activity {
     private Button tekan;
@@ -17,5 +19,26 @@ public class MainActivity extends Activity {
         tekan=(Button)findViewById(R.id.button1);
         grafik1 = new HelloESSurfaceView(this);
         setContentView(grafik1);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        grafik1.onPause();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        grafik1.onResume();
+    }
+    class HelloESSurfaceView extends GLSurfaceView {
+        public HelloESSurfaceView(Context context){
+            super(context);
+            setRenderer(new segitiga());
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main,menu);
+        return true;
     }
 }
